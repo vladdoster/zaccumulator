b81b1c84f3446b21052c5d2fadb6ed6368c6d4be
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-08-31 12:40:54 +0200

    Initial commit – mostly a copy of zzcomplete

---
+++ accumulator.plugin.zsh
@@ -0,0 +1,25 @@
+#
+# No plugin manager is needed to use this file. All that is needed is adding:
+#   source {where-zzcomplete-is}/zzcomplete.plugin.zsh
+#
+# to ~/.zshrc.
+#
+
+0="${(%):-%N}" # this gives immunity to functionargzero being unset
+REPO_DIR="${0%/*}"
+CONFIG_DIR="$HOME/.config/accumulator"
+
+#
+# Update FPATH if:
+# 1. Not loading with Zplugin
+# 2. Not having fpath already updated (that would equal: using other plugin manager)
+#
+
+if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$REPO_DIR]}" != $REPO_DIR ]]; then
+    fpath+=( "$REPO_DIR" )
+fi
+
+autoload zz-process-buffer zz-usetty-wrapper zz-list zz-list-input zz-list-draw zz-list-wrapper accu
+
+zle -N accu
+bindkey '^F' accu
